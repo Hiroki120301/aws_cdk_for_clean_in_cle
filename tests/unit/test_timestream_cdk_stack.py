5eebbe1a2a98003dc75f0d4cac5c45d5e493f3ea
Hiroki120301 <hxn137@case.edu>
2023-09-15 12:55:07 -0400

    complete test cases for aws resources created by aws cdk

--- tests/unit/test_timestream_cdk_stack.py
+++ tests/unit/test_timestream_cdk_stack.py
@@ -3,9 +3,8 @@ import aws_cdk.assertions as assertions
 
 from timestream_cdk.timestream_cdk_stack import TimestreamCdkStack
 
-# example tests. To run these tests, uncomment this file along with the example
-# resource in timestream_cdk/timestream_cdk_stack.py
-def test_sqs_queue_created():
+
+def test_timestream_database_and_table_created():
     app = core.App()
     stack = TimestreamCdkStack(app, "timestream-cdk")
     template = assertions.Template.from_stack(stack)
@@ -27,6 +26,85 @@ def test_sqs_queue_created():
                 "TableName": "airQualityDuplicate"
             },
             "DependsOn": ["AirQualityDuplicate"]
+        }
+    )
+
+
+def test_iam_role_and_policy_created():
+    app = core.App()
+    stack = TimestreamCdkStack(app, "timestream-cdk")
+    template = assertions.Template.from_stack(stack)
+    template.has_resource(
+        "AWS::IAM::Role",
+        {
+            "Properties": {
+                "AssumeRolePolicyDocument": {
+                    "Statement": [
+                        {
+                            "Action": "sts:AssumeRole",
+                            "Effect": "Allow",
+                            "Principal": {
+                                "Service": "iot.amazonaws.com", 
+                            }
+                        },
+                    ]
+                },
+            }
+        }
+    )
+
+
+def test_iot_rule_created():
+    app = core.App()
+    stack = TimestreamCdkStack(app, "timestream-cdk")
+    template = assertions.Template.from_stack(stack)
+    template.has_resource(
+        "AWS::IoT::TopicRule",
+        {
+            "Properties": {
+                "RuleName": "IoT2TimestreamReplicate",
+                "TopicRulePayload": {
+                    "Actions": [
+                        {
+                            "Timestream": {
+                                "DatabaseName": "AirQualityDuplicate",
+                                "Dimensions": [
+                                    {
+                                        "Name": "DeviceEUI",
+                                        "Value": "${dev_eui}",
+                                        }
+                                ],
+                                "RoleArn": {
+                                    "Fn::GetAtt": ["IoTTimestreamRole6C3940E8", "Arn"]
+                                },
+                                "TableName": "airQualityDuplicate",
+                                "Timestamp": {
+                                    "Unit": "MILLISECONDS",
+                                    "Value": "${reported_at}",
+                                }
+                            }
+                        },
+                    ],
+                    "Sql": "SELECT decoded.payload.data.* FROM 'air_quality'",
+                }
+            }
+        }
+    )
+
+
+def test_logs_group_created():
+    app = core.App()
+    stack = TimestreamCdkStack(app, "timestream-cdk")
+    template = assertions.Template.from_stack(stack)
+
+    template.has_resource(
+        "AWS::Logs::LogGroup",
+        {
+            "Properties": {
+                "RetentionInDays": 7, 
+            },
+            "UpdateReplacePolicy": "Retain",
+            "DeletionPolicy": "Retain",
         }
     )
 
